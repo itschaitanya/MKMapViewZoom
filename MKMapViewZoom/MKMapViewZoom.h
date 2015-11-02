b3db6a909aae8da6640538d18cee918ef3817668
Chaitanya Ramavajjala <chaitanyar@zillow.com>
2015-11-01 21:41:28 -0800

    Frameworks.

---
+++ MKMapViewZoom/MKMapViewZoom.h
@@ -0,0 +1,20 @@
+//
+//  MKMapViewZoom.h
+//  MKMapViewZoom
+//
+//  Created by Chaitanya Ramavajjala on 10/23/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MKMapViewZoom.
+FOUNDATION_EXPORT double MKMapViewZoomVersionNumber;
+
+//! Project version string for MKMapViewZoom.
+FOUNDATION_EXPORT const unsigned char MKMapViewZoomVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKMapViewZoom/PublicHeader.h>
+
+
+#import <MKMapViewZoom/MKMapView+ZoomLevel.h>
